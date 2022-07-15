12f339f8358b8107bffc327ed4856c45945de503
Aman Nidhi <31700679+aman-nidhi@users.noreply.github.com>
2022-07-15 10:25:22 +0530

    Update paperba1l_cpp.cpp

--- paperba1l_cpp.cpp
+++ paperba1l_cpp.cpp
@@ -3,50 +3,67 @@
 **/
 #include <bits/stdc++.h>
 using namespace std;
-#define ff first
-#define ss second
-#define pb push_back
-#define pf push_front
-#define mp make_pair
-#define vi vector<int>
-#define pii pair<int, int>
-#define all(vec) vec.begin(), vec.end()
-#define ischar(ch) (ch >= 'a' && ch <= 'z')
-#define isChar(ch) (ch >= 'A' && ch <= 'Z')
-#define mset(vec, x) memset(vec, x, sizeof(vec))
+
+#define ff          first
+#define ss          second
+#define pb          push_back
+#define pf          push_front
+#define mp          make_pair
+#define vi          vector<int>
+#define pii         pair<int,int>
+#define all(v)      (v).begin(),(v).end()
+#define sortv(v)    sort(all(v))
+#define rep(i,a,b)  for(ll i=a;i<=b;i++)
+#define mset(v, x)  memset(v,x,sizeof(vec))
+#define prec(n)     fixed<<setprecision(n)
+#define ischar(ch)  (ch>='a' && ch<='z')
+#define isChar(ch)  (ch>='A' && ch<='Z')
+#define pr(a)       cout<<a<<endl
+#define pr2(a,b)    cout<<a<<" "<<b<<endl
+#define prY         cout<<"YES"<<endl
+#define prN         cout<<"NO"<<endl
+#define si(n)       scanf("%d",&n)
+#define sc(n)       scanf("%c",&n)
+#define sl(n)       scanf("%lld",&n)
+#define sf(n)       scanf("%lf",&n)
 #define boostio                       \
     ios_base::sync_with_stdio(false); \
     cin.tie(0);
 // Input macros
-#define si(n) scanf("%d", &n)
-#define sc(n) scanf("%c", &n)
-#define sl(n) scanf("%lld", &n)
-#define sf(n) scanf("%lf", &n)
 
 typedef long long int ll;
 typedef unsigned long long ull;
+
+inline int gcd(int a, int b) { return (b ? gcd(b, a % b) : a); }
+inline string toString(ll n) {stringstream ss; ss << n;return ss.str();}
+
 const long double EPS   = 1e-9;
 const long double PI    = 3.14159265358979;
-inline int gcd(int a, int b) { return (b ? gcd(b, a % b) : a); }
-inline int lcm(int a, int b) { return a / gcd(a, b) * b; }
-
 const ll mod = 1e9 + 7;
 const ll N = 5000;
 
 //int rrr[]={2,1,-1,-2,-2,-1,1,2};int ccc[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
 //int month[]={31,28,31,30,31,30,31,31,30,31,30,31}; //month
 
-void solve() {
+void solve(int t) {
+    int n, m;
+    cin >> n >> m;
+    
+    int a;
+    rep(i, 0, n-1) {
+        cin>>a;
+    }
     
     return;
 }
+
 int main() {
     boostio;
-    int tcs = 1;
+    int t = 1;
 
-    // cin>>tcs;
-    while (tcs--){
-        solve();
+    // cin>>t;
+    while (t--){
+        solve(t);
     }
     return 0;
 }
