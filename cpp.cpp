eb46f07f6e6e6a1f78f4bfabcecc0b74de869486
Aman Nidhi <31700679+aman-nidhi@users.noreply.github.com>
2023-11-06 22:17:39 +0530

    Update cpp.cpp

--- cpp.cpp
+++ cpp.cpp
@@ -1,6 +1,4 @@
-/**
- *  @author:      paperba1l
-**/
+/** @author: paperba1l **/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,14 +9,12 @@ using namespace std;
 #define vii         vector<pii>
 #define vl          vector<ll>
 #define all(v)      ((v).begin(),(v).end())
-#define sortv(v)    sort(all(v))
-#define rep(i,a,b)  for(int i=a;i<=b;i++)
 #define mset(ar,x)  memset(ar,x,sizeof(ar))
 #define prec(n)     fixed<<setprecision(n)
 #define ischar(ch)  (ch>='a' && ch<='z')
 #define isChar(ch)  (ch>='A' && ch<='Z')
-#define print(a)    cout<<a<<endl
-#define printt(a,b) cout<<a<<" "<<b<<endl
+#define print(a)    cout<<a<<endl;
+#define print2(a,b) cout<<a<<" "<<b<<endl;
 #define boostio                       \
     ios_base::sync_with_stdio(false); \
     cin.tie(0);
@@ -30,31 +26,14 @@ typedef unsigned long long ull;
 inline int gcd(int a, int b) { return (b ? gcd(b, a % b) : a); }
 inline string toString(ll n) {stringstream ss; ss << n;return ss.str();}
 
-const long double EPS   = 1e-9;
-const long double PI    = 3.14159265358979;
-
 //int rrr[]={2,1,-1,-2,-2,-1,1,2};int ccc[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
 //int month[]={31,28,31,30,31,30,31,31,30,31,30,31}; //month
 
-const ll mod = 1e9+7;
+const ll MOD = 1e9+7;
 const ll N = 5000;
-
 void solve(int __tc) {
-    int a, b, c, d, n, m;
     
-    
-    n = 1;
-    rep(i, 0, n-1) {
-        
-    }
-
-    
-    int ans = 0;
-    
-    
-    
-    print("ans");
-    
+    print("a")
     return;
 }
 
